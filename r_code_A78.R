a6e85afb1abea61056620d4de9bd4836370d10ce
Muhammad Usman <mu24aan@herts.ac.uk>
2024-12-30 07:36:38 -0800

    load dataset and check dataset columns and rows in RStudio using rcode

---
+++ r_code_A78.R
@@ -0,0 +1,4 @@
+data <- read.csv("dataset/est13us.csv") #Load the Dataset into RStudio
+head(data)        # View the first few rows
+str(data)         # View the structure of the dataset
+summary(data)     # View summary statistics
